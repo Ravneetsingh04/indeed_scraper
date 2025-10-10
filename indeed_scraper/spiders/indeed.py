eba67c3d97c2ce20568f1dc76f7622897e427b7d
Ravneetsingh04 <114427225+Ravneetsingh04@users.noreply.github.com>
2025-10-10 11:50:19 -0400

    added [:5]

--- indeed_scraper/spiders/indeed.py
+++ indeed_scraper/spiders/indeed.py
@@ -52,7 +52,7 @@ class IndeedSpider(scrapy.Spider):
         else:
             self.log(f"✅ Found {len(job_cards)} job cards.")
 
-        for card in job_cards:
+        for card in job_cards[:5]:
             title = (
                 card.css("h2.jobTitle span::text").get()
                 or card.css("h2 span::text").get()
