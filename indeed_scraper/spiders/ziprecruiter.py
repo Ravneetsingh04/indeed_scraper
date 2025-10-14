cddb7e794679b993d15ebc6b9b492331eb292c38
Ravneetsingh04 <114427225+Ravneetsingh04@users.noreply.github.com>
2025-10-14 14:18:00 -0400

    Update ziprecruiter.py

--- indeed_scraper/spiders/ziprecruiter.py
+++ indeed_scraper/spiders/ziprecruiter.py
@@ -56,7 +56,7 @@ class ZipRecruiterSpider(scrapy.Spider):
         else:
             self.log(f"✅ Found {len(job_cards)} job cards.")
     
-        for card in job_cards[:5]:
+        for card in job_cards[:10]:
             # --- Job Title ---
             title = (
                 card.css("h2::text").get()
