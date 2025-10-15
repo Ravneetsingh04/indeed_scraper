71b9f01c84686b66aa12493dca84463e06784618
Ravneetsingh04 <114427225+Ravneetsingh04@users.noreply.github.com>
2025-10-15 15:12:11 -0400

    Update ziprecruiter.py

--- indeed_scraper/spiders/ziprecruiter.py
+++ indeed_scraper/spiders/ziprecruiter.py
@@ -22,7 +22,7 @@ class ZipRecruiterSpider(scrapy.Spider):
         self.seen_urls = set()
 
     def start_requests(self):
-        search_query = "Python Developer"
+        search_query = "React Developer"
         search_location = "New York, NY"
 
         # ZipRecruiter Search URL
